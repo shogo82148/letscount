33b9b5fb07bdbb78af36068dc2cfeaa862f6397a
Ichinose Shogo <shogo82148@gmail.com>
2012-09-10 23:07:49 +0900

    first commit

---
+++ count.js
@@ -0,0 +1,88 @@
+var PERIOD_SHOW = 200;
+var SLEEP_TIME = 0;
+
+addEventListener('message', onMessage, false);
+
+function onMessage(e) {
+    var data = e.data;
+    countRoute(data.rows, data.cols);
+}
+
+function countRoute(rows, cols) {
+    var count = 0; // 見つけたルートの数
+    var path = []; // これまでに通った経路
+    var visited = []; // 訪問履歴
+    var i, j;
+
+    if(rows<=3 && cols<=3) {
+        PERIOD_SHOW = 0;
+        SLEEP_TIME = 100;
+    } else if(rows==4 && cols==4) {
+        PERIOD_SHOW = 0;
+        SLEEP_TIME = 10;
+    }
+
+    //初期化
+    var tmp;
+    for(i=0;i<=cols;i++) {
+        tmp = [];
+        for(j=0;j<=rows;j++) {
+            tmp.push(false);
+        }
+        visited.push(tmp);
+    }
+
+    // 検索
+    search(0, 0);
+
+    // 結果出力
+    postMessage({
+        count: count
+    });
+
+    function search(x, y) {
+        //postMessage(x + ', ' + y);
+
+        //範囲外判定
+        if(x<0 || x>cols || y<0 || y>rows) return;
+
+        //訪問済み判定
+        if(visited[x][y]) return;
+
+        path.push([x, y]);
+
+        //ゴール！
+        if(x==cols && y==rows) {
+            ++count;
+            showPath(count, path);
+            path.pop();
+            return;
+        }
+
+        //検索を続行
+        visited[x][y] = true;
+        search(x+1, y);
+        search(x, y+1);
+        search(x-1, y);
+        search(x, y-1);
+
+        visited[x][y] = false;
+        path.pop();
+    }
+}
+
+var lastShowTime = (new Date()).getTime();
+function showPath(count, path) {
+    var now = (new Date()).getTime();
+    if(now - lastShowTime < PERIOD_SHOW) return;
+    lastShowTime = now;
+
+    while(now - lastShowTime < SLEEP_TIME) {
+        now = (new Date()).getTime();
+    }
+    postMessage({
+        path: path,
+        count: count
+    });
+    lastShowTime = now;
+}
---
+++ letscount.js
@@ -0,0 +1,145 @@
+$(function() {
+    var canvas = $('#path');
+    var width = canvas.attr('width') * 1;
+    var height = canvas.attr('height') * 1;
+    var margin = 10;
+    var ctx = canvas[0].getContext('2d');
+    var worker;
+
+    function start(rows, cols) {
+        // 古いワーカーは用済み
+        if(worker) {
+            worker.terminate();
+        }
+        if(rows<=0 || cols<=0) return ;
+
+        // 画面更新
+        $('#problem-text').text(rows + '×'+  cols);
+
+        // 新しいワーカーを作成・初期化
+        worker = new Worker('count.js');
+        worker.addEventListener('message', onMessage, false);
+        worker.postMessage({rows: rows, cols: cols});
+
+        var xstep = (width - 2*margin) / cols;
+        var ystep = (height - 2*margin) / rows;
+        var resultText = $('#result-text');
+
+        drawAllPath();
+        $('#start, #goal').show();
+
+        // パス表示
+        function onMessage(e) {
+            var data = e.data;
+            drawAllPath();
+            if(data.path) {
+                drawPath(data.path);
+            }
+            if(data.count) {
+                showCount(data.count);
+            }
+        }
+
+        // 描画
+        function drawAllPath() {
+            var i, p1, p2;
+            ctx.beginPath();
+            ctx.clearRect(0, 0, width, height);
+            ctx.lineWidth = 3;
+            ctx.strokeStyle = 'black';
+
+            //横線
+            for(i=0;i<=rows;i++) {
+                p1 = toScreen(0, i);
+                p2 = toScreen(cols, i);
+
+                ctx.moveTo(p1.x, p1.y);
+                ctx.lineTo(p2.x, p2.y);
+            }
+
+            //縦線
+            for(i=0;i<=cols;i++) {
+                p1 = toScreen(i, 0);
+                p2 = toScreen(i, rows);
+
+                ctx.moveTo(p1.x, p1.y);
+                ctx.lineTo(p2.x, p2.y);
+            }
+            ctx.stroke();
+        }
+
+        // パスの描画
+        function drawPath(path) {
+            ctx.lineWidth = 7;
+            ctx.strokeStyle = '#d24e63';
+            ctx.beginPath();
+            var i, p;
+            p = toScreen(path[0][0], path[0][1]);
+            ctx.moveTo(p.x, p.y);
+            for(i=1;i<path.length;i++) {
+                p = toScreen(path[i][0], path[i][1]);
+                ctx.lineTo(p.x, p.y);
+            }
+            ctx.stroke();
+        }
+
+        // 経路数の表示
+        function showCount(count) {
+            var s = '';
+            if(count >= 1E12) {
+                s = join(s, Math.floor(count / 1E12)) + '兆';
+                count %= 1E12;
+            }
+            if(count >= 100000000) {
+                s = join(s, Math.floor(count / 1E8)) + '億';
+                count %= 1E8;
+            }
+            if(count >= 1E4) {
+                s = join(s, Math.floor(count / 1E4)) + '万';
+                count %= 1E4;
+            }
+            s = join(s, count);
+            resultText.text(s);
+
+            function join(s, count) {
+                if(s=='' || count>=1000) {
+                    return s + count;
+                } else if(count>=100) {
+                    return s + '0' + count;
+                } else if(count>=10) {
+                    return s + '00' + count;
+                }
+                return s + '000' + count;
+            }
+        }
+
+        // 画面上の位置を計算
+        function toScreen(x, y) {
+            return {
+                x: x * xstep + margin,
+                y: y * ystep + margin
+            };
+        }
+    }
+
+    function resize() {
+        var canvas = $('#path');
+        var pos = canvas.offset();
+        var scale = canvas.width() / canvas.attr('width');
+        $('#start').css({
+            top: pos.top + margin * scale,
+            left: pos.left + margin * scale
+        });
+        $('#goal').css({
+            top: pos.top + canvas.height() - margin * scale,
+            left: pos.left + canvas.width() - margin * scale
+        });
+    }
+
+    $(window).resize(resize);
+    resize();
+
+    $('input').click(function() {
+        start($(this).attr('rows')*1, $(this).attr('cols')*1);
+    });
+});
